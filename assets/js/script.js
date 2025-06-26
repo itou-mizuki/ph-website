ea5876e765726b90742f300d523d3a080980ad26
itou-mizuki <mizukibulu1818@gmail.com>
2025-06-26 13:17:18 +0900

    変更

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,43 @@ let li = document.createElement("li");
 li.textContent = "テスト見出し";
 let div = document.getElementById("test");
 div.appendChild(li);
+let header = document.getElementsByClassName("main-header")[0];
 const button = document.getElementById("button");
-const menu = document.getElementsByClassName(".nav");
+const menu = document.getElementsByClassName("nav")[0];
+const body = document.body;
 button.addEventListener("click", function () {
   menu.classList.toggle("nav-js");
+  body.classList.toggle("no-scroll");
+  header.classList.toggle("is-open");
+  header.classList.toggle("main-header-clear");
 });
+let mainvisual = document.getElementById("main-visual");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > mainvisual.clientHeight - header.clientHeight) {
+    header.classList.remove("main-header-clear");
+  } else {
+    header.classList.add("main-header-clear");
+  }
+});
+
+new Splide("#slider1", {
+  type: "loop",
+  perPage: 3,
+  width: "1096px",
+  gap: "40px",
+  padding: "28px",
+  perMove: 1,
+  breakpoints: {
+    600: {
+      perPage: 1,
+    },
+  },
+}).mount();
+
+new Splide("#slider2", {
+  type: "loop",
+  perPage: 1,
+  width: "80vw",
+  padding: "28px",
+  gap: "20px",
+}).mount();
